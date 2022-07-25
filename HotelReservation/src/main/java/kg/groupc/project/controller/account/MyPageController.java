1542f09e022bb0685e35de012839c3e191a5477d
jyKim <87361154+kjy4926@users.noreply.github.com>
2022-07-26 02:29:46 +0900

    mypage 및 리뷰 페이지 구현 중
    마이페이지 메인 리스트 출력 구현 완료
    리뷰 작성 페이지 구현 완료
    ------------------------------
    해야할 일
    리뷰페이지 백엔드 동작 구현
    마이페이지 -> 상세 보기 리스트 구현

--- HotelReservation/src/main/java/kg/groupc/project/controller/account/MyPageController.java
+++ HotelReservation/src/main/java/kg/groupc/project/controller/account/MyPageController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,23 +24,30 @@ import kg.groupc.project.controller.BaseController;
 import kg.groupc.project.dto.account.BookingDto;
 import kg.groupc.project.dto.account.InfoChangeFormDto;
 import kg.groupc.project.dto.account.PwdChangeFormDto;
+import kg.groupc.project.dto.account.StarsDto;
 import kg.groupc.project.entity.account.Account;
 import kg.groupc.project.entity.hotel.Booking;
 import kg.groupc.project.entity.hotel.Room;
 import kg.groupc.project.entity.restaurant.Restaurant;
+import kg.groupc.project.entity.restaurant.Stars;
+import kg.groupc.project.repository.restaurant.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class MyPageController extends BaseController{
+	//test
+	private final RestaurantRepository<Restaurant, Long> rr;
 	private final PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/mypage")
 	public String mypage(@AuthenticationPrincipal User user, Model model) {
 		String userId = user.getUsername();
 		List<ArrayList<BookingDto>> bookingList = accountService.getBookingList(userId);
+		List<StarsDto> starsList = accountService.getStarsList(userId);
 		model.addAttribute("reserveBookingList", bookingList.get(0));
 		model.addAttribute("progressedBookingList", bookingList.get(1));
+		model.addAttribute("starsDtoList", starsList);
 		return "/mypage/mypage";
 	}
 	
@@ -128,6 +136,17 @@ public class MyPageController extends BaseController{
 		return accountService.resignAccount(user.getUsername());
 	}
 	
+	@GetMapping("/mypage/review/hotel/write/{seq}")
+	public String hotelReview(@PathVariable Long seq, Model model) {
+		return "/mypage/review/reviewForm";
+	}
+	
+	@GetMapping("/mypage/review/restaurant/write/{seq}")
+	public String restaurantReview(@PathVariable Long seq, Model model) {
+		return "/mypage/review/reviewForm";
+	}
+	
+	//test page
 	@GetMapping("/test/createBook")
 	public String createTestBooking(@AuthenticationPrincipal User user) {
 		Booking booking = new Booking();
@@ -148,7 +167,25 @@ public class MyPageController extends BaseController{
 	
 	@GetMapping("/test/createStar")
 	public String createTestStar(@AuthenticationPrincipal User user) {
-		Restaurant restaurant = new Restaurant();
-		return "/";
+		Account account = accountService.getAccountById(user.getUsername());
+		Restaurant r1 = rr.findById(24L).get();
+		Restaurant r2 = rr.findById(25L).get();
+		Restaurant r3 = rr.findById(26L).get();
+		Stars s1 = new Stars();
+		Stars s2 = new Stars();
+		Stars s3 = new Stars();
+		
+		s1.setUserId(account);
+		s1.setRestaurant(r1);
+		s2.setUserId(account);
+		s2.setRestaurant(r2);
+		s3.setUserId(account);
+		s3.setRestaurant(r3);
+		
+//		starsService.saveStars(s1);
+//		starsService.saveStars(s2);
+//		starsService.saveStars(s3);
+		
+		return "redirect:/";
 	}
 }
